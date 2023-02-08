9512b3974203748defe3cd9b71b466f24928e3bb
marcos.silvestrini <marcos.silvestrini@totvs.com.br>
2023-02-08 16:44:45 -0300

    Set name with space

--- build/generate_list_certificates.ps1
+++ build/generate_list_certificates.ps1
@@ -5,7 +5,8 @@
     Dependencies: none
 #>
 $ROOT_PATH = $PSScriptRoot | Split-Path -Parent
-$LIST_FILES = (Get-ChildItem -Recurse "$ROOT_PATH\certificates" -Include @("*.pdf", "*.jpg") |
+$LIST_FILES = (Get-ChildItem -Recurse "$ROOT_PATH\certificates" -Include @("*.pdf", "*.jpg")  |
+    Rename-Item -NewName { $_.Name -replace ' ', '_' } |
     Where-object { $_.Name -notlike ".gitkeep" -and !$_.PSISContainer }).FullName
 
 $FILES = $LIST_FILES -replace ("F:\\Projetos\\learning-path\\", "")
