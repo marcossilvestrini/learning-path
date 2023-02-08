2cd0bf9fad88492fd3f465d26bfe2edffe07353c
marcos.silvestrini <marcos.silvestrini@totvs.com.br>
2023-02-08 16:34:34 -0300

    Update scripts

---
+++ build/generate_list_certificates.ps1
@@ -0,0 +1,13 @@
+<#
+    Author: Marcos Silvestrini
+    Date: 08/02/2023
+    Description: Script for create a list of all certificates presents in folder certificates
+    Dependencies: none
+#>
+$ROOT_PATH = $PSScriptRoot | Split-Path -Parent
+$LIST_FILES = (Get-ChildItem -Recurse "$ROOT_PATH\certificates" -Include @("*.pdf", "*.jpg") |
+    Where-object { $_.Name -notlike ".gitkeep" -and !$_.PSISContainer }).FullName
+
+$FILES = $LIST_FILES -replace ("F:\\Projetos\\learning-path\\", "")
+$FILES = $FILES -replace ("\\", "/")
+$FILES > "$ROOT_PATH\build\certificates"
