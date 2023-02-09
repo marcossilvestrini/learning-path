4c13464db24bec505459db67e056e32ef4882d2c
marcos.silvestrini <marcos.silvestrini@totvs.com.br>
2023-02-09 10:45:52 -0300

    Update files

--- build/generate_list_certificates.ps1
+++ build/generate_list_certificates.ps1
@@ -4,7 +4,6 @@
     Description: Script for create a list of all certificates presents in folder certificates
     Dependencies: none
 #>
-Start-Sleep 10
 $ROOT_PATH = $PSScriptRoot | Split-Path -Parent
 $LIST_FILES = Get-ChildItem -Recurse "$ROOT_PATH\certificates" -Include @("*.pdf", "*.jpg")  |
 Where-object { $_.Name -notlike ".gitkeep" -and !$_.PSISContainer }
